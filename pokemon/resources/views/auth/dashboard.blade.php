2387443fb7461ec67e198a2575c8438175ca832c
Kingstom0113 <manuelsr0113@gmail.com>
2024-01-17 16:56:05 +0100

    .

--- pokemon/resources/views/auth/dashboard.blade.php
+++ pokemon/resources/views/auth/dashboard.blade.php
@@ -22,30 +22,35 @@
                 <td>{{ $pokemon->region }}</td>
                 <td>
                     <a href="{{ route('editar_pokemon', ['id' => $pokemon->id]) }}" class="btn btn-primary">Editar</a>
-                     <button type="button" class="btn btn-danger" data-toggle="modal" data-target="#confirm-delete-modal-{{ $pokemon->id }}">Eliminar</button>
-                      <!-- Modal -->
-                    <div class="modal fade" id="confirm-delete-modal-{{ $pokemon->id }}" tabindex="-1" role="dialog" aria-labelledby="exampleModalCenterTitle" aria-hidden="true">
-                        <div class="modal-dialog modal-dialog-centered" role="document">
-                            <div class="modal-content">
-                                <div class="modal-header">
-                                    <h5 class="modal-title" id="exampleModalLongTitle">Confirmar eliminación</h5>
-                                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                                        <span aria-hidden="true">&times;</span>
-                                    </button>
-                                </div>
-                                <div class="modal-body">
-                                    ¿Estás seguro de que quieres eliminar a {{ $pokemon->name }}?
-                                </div>
-                                <div class="modal-footer">
-                                    <button type="button" class="btn btn-secondary" data-dismiss="modal">Cancelar</button>
-                                    <a href="{{ route('eliminar_pokemon', ['id' => $pokemon->id]) }}" class="btn btn-danger">Eliminar</a>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
+                    <button type="button" class="btn btn-danger" data-bs-toggle="modal" data-bs-target="#ConfirmDelete">
+                        Eliminar
+                    </button>
+
                 </td>
             </tr>
         @endforeach
     </table>
+    <!-- Modal -->
+    <div class="modal fade" id="ConfirmDelete" tabindex="-1" aria-labelledby="ConfirmDelete" aria-hidden="true">
+    <div class="modal-dialog">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h5 class="modal-title" id="ConfirmDelete">¿Deseas eliminar a este Pokémon?</h5>
+                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+            </div>
+            <div class="modal-body">
+                Si se elimina, no volverá a aparecer nunca <i class="bi bi-emoji-frown-fill"></i>
+            </div>
+            <div class="modal-footer">
+                <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Volver</button>
+                <form method="post" id="deleteForm" action="{{ route('eliminar_pokemon', ['id' => $pokemon->id]) }}">
+                    @csrf
+                    @method('DELETE')
+                    <button type="submit" class="btn btn-danger">Eliminar</button>
+                </form>
+            </div>
+        </div>
+    </div>
+</div>
     <a href="{{ route('formulario_pokemon') }}" class="btn btn-primary center">Añadir Pokemon</a>
 @endsection
