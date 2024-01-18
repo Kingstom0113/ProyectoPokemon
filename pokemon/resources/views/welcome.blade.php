fd25368d821f77acddf3a97f50b960fe89f3ff6e
Manuel Soto <manuelsr0113@gmail.com>
2024-01-18 10:38:52 +0100

    implemento sass

--- pokemon/resources/views/welcome.blade.php
+++ pokemon/resources/views/welcome.blade.php
@@ -6,28 +6,47 @@
     <title>Pokémon Landing Page</title>
     @vite(['resources/js/app.js','resources/css/app.css', 'resources/css/app.scss'])
 </head>
-<body>
+<body class="body-welcome">
     <!-- Barra de Navegación -->
-    <nav class="navbar navbar-expand-lg navbar-light">
-        <div class="container">
-            <div class="collapse navbar-collapse" id="navbarNav">
-                @if (Route::has('login'))
-    <div class="fixed-top p-3 text-end">
-        @auth
-            <a href="{{ url('/home') }}" class="btn btn-outline-secondary">PC</a>
-        @else
-            <a href="{{ route('login') }}" class="btn btn-outline-primary">Iniciar sesión</a>
+    <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
+            <div class="container">
+                <a class="navbar-brand" href="{{ url('/') }}">
+                    {{ config('Usuario', 'Pokemaniaco') }}
+                </a>
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
 
-            @if (Route::has('register'))
-                <a href="{{ route('register') }}" class="btn btn-primary ms-2">Registrarse</a>
-            @endif
-        @endauth
-    </div>
-@endif
-
+                <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                    <!-- Right Side Of Navbar -->
+                    <ul class="navbar-nav">
+                        <!-- Authentication Links -->
+                         @guest
+                            <li class="nav-item">
+                                <a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a>
+                            </li>
+                            @if (Route::has('register'))
+                                <li class="nav-item">
+                                    <a class="nav-link" href="{{ route('register') }}">{{ __('Register') }}</a>
+                                </li>
+                            @endif
+                        @else
+                            <div class="dropdown">
+                                <button class="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenuButton1" data-bs-toggle="dropdown" aria-expanded="false">
+                                    {{ Auth::user()->name }}
+                                </button>
+                                <ul class="dropdown-menu" aria-labelledby="dropdownMenuButton1">
+                                  <li><a class="dropdown-item" href="{{ route('logout') }}" onclick="event.preventDefault(); document.getElementById('logout-form').submit();">{{ __('Logout') }}</a></li>
+                                </ul>
+                                <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
+                                    @csrf
+                                </form>
+                            </div>
+                        @endguest
+                    </ul>
+                </div>
             </div>
-        </div>
-    </nav>
+        </nav>
     <footer class="bg-dark text-center p-3 text-light">
         <p>&copy; {{ date('Y') }} Pokémon. Todos los derechos reservados.</p>
     </footer>
